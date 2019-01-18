3f5b0980711d8ba9a816bfb1564c83fd62ca97bb
Drew Cook <drew@linkrightmedia.com>
2019-01-18 11:33:13 -0700

    add in firebase

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 
+// database setup
+require("./database/firebaseConfig");
+
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
---
+++ database/firebaseConfig.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+	apiKey: "<YOUR-API-KEY>",
+	authDomain: "<YOUR-AUTH-DOMAIN>",
+	databaseURL: "<YOUR-DATABASE-URL>",
+	storageBucket: "<YOUR-STORAGE-BUCKET>"
+};
+
+firebase.initializeApp(firebaseConfig);
