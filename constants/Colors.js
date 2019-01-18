2da333cff0d7cdecebf6b2e831a5812349f65a90
Drew Cook <drew@linkrightmedia.com>
2019-01-17 23:06:28 -0700

    add in facebook login, update icons and home screen

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,19 @@
 const tintColor = '#2f95dc';
 
-export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+const colors = {
+	primary: "#0d7095",
+	secondary: "#aeef52",
+	ice: "#9eedf9",
+	tintColor,
+	tabIconDefault: '#ccc',
+	tabIconSelected: tintColor,
+	tabBar: '#fefefe',
+	errorBackground: 'red',
+	errorText: '#fff',
+	warningBackground: '#EAEB5E',
+	warningText: '#666804',
+	noticeBackground: tintColor,
+	noticeText: '#fff',
 };
+
+export default colors;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,188 +1,150 @@
 import React from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+	Image,
+	Platform,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
 } from 'react-native';
-import { WebBrowser } from 'expo';
-
+import colors from "../constants/Colors";
+import { AuthSession } from "expo";
+//import { WebBrowser } from 'expo';
 import { MonoText } from '../components/StyledText';
 
+const FB_APP_ID = "566780290461704";
+
 export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+	state = {
+		result: null,
+		userInfo: null,
+	};
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
+	render() {
+		return (
+			<View style={styles.container}>
+				<ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+					<View style={styles.welcomeContainer}>
+						<Image
+							source={require('../assets/images/main_icon.png')}
+							style={styles.welcomeImage}
+						/>
+					</View>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
+					<View style={styles.getStartedContainer}>
+						<Text style={styles.getStartedText}>
+							Welcome to Mountain Chat!
+						</Text>
+						{!this.state.userInfo ? (
+								<TouchableOpacity style={styles.logInBtn} onPress={this._handleFBLogin}>
+									<Text style={styles.logInBtnText}>Log In With Facebook</Text>
+								</TouchableOpacity>
+							) :
+							(this._renderUserInfo())
+						}
+					</View>
+				</ScrollView>
+			</View>
+		);
+	}
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+	_handlePressAsync = async () => {
+		let redirectUrl = AuthSession.getRedirectUrl();
+		let result = await AuthSession.startAsync({
+			authUrl:
+			`https://www.facebook.com/v2.8/dialog/oauth?response_type=token` +
+			`&client_id=${FB_APP_ID}` +
+			`&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+		});
+		this.setState({ result });
+	};
 
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
+	_handleFBLogin = async () => {
+		try {
+			const {
+				type,
+				token,
+				expires,
+				permissions,
+				declinedPermissions,
+			} = await Expo.Facebook.logInWithReadPermissionsAsync(FB_APP_ID, {
+				permissions: ['public_profile'],
+			});
+			if (type === 'success') {
+				// Get the user's name using Facebook's Graph API
+				const response = await fetch(`https://graph.facebook.com/me?access_token=${token}&fields=id,name,picture.type(large)`);
+				const userInfo = await response.json();
+				this.setState({userInfo});
+			} else {
+				// type === 'cancel'
+			}
+		} catch ({ message }) {
+			alert(`Facebook Login Error: ${message}`);
+		}
+	}
 
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
+	_renderUserInfo = () => {
+		return (
+			<View style={{ aligntItems: "center" }}>
+				<Image
+					source={{ uri: this.state.userInfo.picture.data.url }}
+					style={{ width: 100, height: 100, borderRadius: 50 }}
+				/>
+				<Text style={{ fontSize: 20 }}>Hello, {this.state.userInfo.name}!</Text>
+				<Text>ID: {this.state.userInfo.id}</Text>
+			</View>
+		);
+	};
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: colors.primary,
+	},
+	developmentModeText: {
+		marginBottom: 20,
+		color: 'rgba(0,0,0,0.4)',
+		fontSize: 14,
+		lineHeight: 19,
+		textAlign: 'center',
+	},
+	contentContainer: {
+		paddingTop: 30,
+	},
+	welcomeContainer: {
+		alignItems: 'center',
+		marginTop: 10,
+		marginBottom: 20,
+	},
+	welcomeImage: {
+		width: 200,
+		height: 160,
+		resizeMode: 'contain',
+		marginTop: 30,
+		marginLeft: -10,
+	},
+	getStartedContainer: {
+		alignItems: 'center',
+		marginHorizontal: 50,
+	},
+	getStartedText: {
+		fontSize: 30,
+		color: "#fff",
+		lineHeight: 32,
+		textAlign: 'center',
+	},
+	logInBtn: {
+		textAlign: 'center',
+		backgroundColor: colors.ice,
+		paddingVertical: 15,
+		paddingHorizontal: 25,
+		marginTop: 60,
+		borderRadius: 4,
+	},
+	logInBtnText: {
+		color: "rgba(0, 0, 0, 0.8)",
+		fontSize: 20,
+	},
 });
