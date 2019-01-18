3fb4cf7c9b316fc7e01ee2895a23be29096d50ec
Drew Cook <drew@linkrightmedia.com>
2019-01-18 02:20:05 -0700

    add in mountain images and header

--- constants/mountains.js
+++ constants/mountains.js
@@ -2,26 +2,26 @@ export const mountains = [
 	{
 		name: "Beaver Creek",
 		region: "Colorado, USA",
-		image: "app/assets/images/mountains/beaver_creek.jpg",
+		image: require("app/assets/images/mountains/beaver_creek.jpg"),
 	},
 	{
 		name: "Breckenridge",
 		region: "Colorado, USA",
-		image: "app/assets/images/mountains/breckenridge.jpg",
+		image: require("app/assets/images/mountains/breckenridge.jpg"),
 	},
 	{
 		name: "Keystone",
 		region: "Colorado, USA",
-		image: "app/assets/images/mountains/keystone.jpg",
+		image: require("app/assets/images/mountains/keystone.jpg"),
 	},
 	{
 		name: "Snowbird",
 		region: "Utah, USA",
-		image: "app/assets/images/mountains/snowbird.jpg",
+		image: require("app/assets/images/mountains/snowbird.jpg"),
 	},
 	{
 		name: "Vail",
 		region: "Colorado, USA",
-		image: "app/assets/images/mountains/vail.jpg",
+		image: require("app/assets/images/mountains/vail.jpg"),
 	},
 ];
--- screens/MountainsScreen.js
+++ screens/MountainsScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
 	FlatList,
+	Image,
 	Platform,
 	ScrollView,
 	StyleSheet,
@@ -16,12 +17,14 @@ export default class MountainsScreen extends React.Component {
 	render() {
 		return (
 			<ScrollView style={styles.container}>
+				<Text style={styles.heading}>Mountians List</Text>
 				<FlatList
 					data={mountains}
 					keyExtractor={(item, idx) => idx.toString()}
 					renderItem={(item, idx) => (
 						<TouchableOpacity onPress={() => this._onPress(item.item.name)}>
 							<View style={styles.mountainContainer}>
+								<Image source={item.item.image} style={styles.mountainImg} />
 								<Text style={styles.mountainText}>
 									{item.item.name}
 								</Text>
@@ -45,8 +48,13 @@ export default class MountainsScreen extends React.Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		paddingTop: 15,
+		paddingTop: 20,
 		backgroundColor: '#fff',
+	},
+	heading: {
+		fontSize: 38,
+		marginBottom: 20,
+		textAlign: "center",
 	},
 	mountainContainer: {
 		paddingVertical: 10,
@@ -54,17 +62,29 @@ const styles = StyleSheet.create({
 		backgroundColor: "#efefef",
 		flex: 1,
 		flexDirection: "row",
+		alignItems: "center",
 		justifyContent: "space-between",
 		borderWidth: 0.5,
 		borderTopColor: "#aaa",
 		borderBottomColor: "#aaa",
 	},
+	mountainImg: {
+		borderRadius: 40,
+		width: 80,
+		height: 80,
+		borderColor: "#aaa",
+		borderWidth: 1,
+		marginRight: 15,
+	},
 	mountainText: {
 		color: "#000",
-		fontSize: 20,
+		fontSize: 28,
+		lineHeight: 28,
+		flexGrow: 2,
 	},
 	mountainArrow: {
 		color: "#000",
-		fontSize: 26,
+		fontSize: 30,
+		marginBottom: -3,
 	}
 });
