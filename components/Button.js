f413fcb11305a79bce331f9a5d481166d59deced
Drew Cook <drew@linkrightmedia.com>
2019-03-05 10:16:04 -0700

    button updates

--- components/Button.js
+++ components/Button.js
@@ -1,17 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {
 	Text,
 	TouchableOpacity,
 } from "react-native";
 import colors from "../constants/Colors";
 
-export default Button = props => (
-	<TouchableOpacity style={styles(props.bgColor).btnContainer} onPress={props.onPress}>
-		<Text style={styles(props.bgColor).btnText}>{props.title}</Text>
+const Button = props => (
+	<TouchableOpacity style={styles(props.bgColor, props.btnStyle, null).btnContainer} onPress={props.onPress}>
+		<Text style={styles(props.bgColor, null, props.textStyle).btnText}>{props.title}</Text>
 	</TouchableOpacity>
 );
 
-const styles = (bgColor) => ({
+Button.propTypes = {
+	title: PropTypes.string.isRequired,
+	onPress: PropTypes.func.isRequired,
+	bgColor: PropTypes.string,
+	btnStyle: PropTypes.object,
+	textStyle: PropTypes.object,
+};
+
+const styles = (bgColor, btnStyle, textStyle) => ({
 	btnContainer: {
 		backgroundColor: bgColor ? bgColor : "#333",
 		paddingVertical: 15,
@@ -19,11 +28,15 @@ const styles = (bgColor) => ({
 		marginVertical: 20,
 		//marginHorizontal: 10,
 		borderRadius: 4,
+		...btnStyle,
 	},
 	btnText: {
 		color: "rgba(255, 255, 255, 0.95)",
 		fontSize: 18,
 		textAlign: "center",
 		textTransform: "uppercase",
+		...textStyle,
 	},
 });
+
+export default Button;
--- store/actions/mountainsActions.js
+++ store/actions/mountainsActions.js
@@ -2,22 +2,24 @@ export const getMountains = () => {
 	return (dispatch, getState, {getFirebase, getFirestore}) => {
 		dispatch({type: "GET_MOUNTAINS_PENDING"});
 		const firestore = getFirestore();
-		const firebase = getFirebase();
-		firestore.collection("mountains").orderBy("name", "asc").get().then(async docs => {
-			let mountains = [];
-			await docs.forEach(async doc => {
-				const {name, region, logo, chatroomUsers} = doc.data();
-				//const logoRef = firebase.storage().ref(`/mountains/logos/${logo}`);
-				//const logoUrl = await logoRef.getDownloadURL();
-				mountains.push({
-					id: doc.id,
-					name,
-					region,
-					logo,
-					chatroomUsers,
-				});
+		//const firebase = getFirebase();
+		firestore.collection("mountains").orderBy("name", "asc").get()
+			.then(docs => {
+				let mountains = [];
+				docs.forEach(doc => {
+					const {name, region, logo, chatroomUsers} = doc.data();
+					//const logoRef = firebase.storage().ref(`/mountains/logos/${logo}`);
+					//const logoUrl = await logoRef.getDownloadURL();
+					mountains.push({
+						id: doc.id,
+						name,
+						region,
+						logo,
+						chatroomUsers,
+					});
+				})
+				dispatch({type: "GET_MOUNTAINS_SUCCESS", payload: mountains});
 			})
-			dispatch({type: "GET_MOUNTAINS_SUCCESS", payload: mountains});
-		}).catch(err => dispatch({type: "GET_MOUNTAINS_FAIL", err}));
+			.catch(err => dispatch({type: "GET_MOUNTAINS_FAIL", err}));
 	};
 }
